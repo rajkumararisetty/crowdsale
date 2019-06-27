7c60fb153e0d6871762b14eb7c2c03fe969a4bdd
rajkumararisetty <rajkumararisetty1993@gmail.com>
2019-06-27 23:58:10 +0530

    Created Withdraw and Deposit ether contract functions

--- src/contracts/Exchange.sol
+++ src/contracts/Exchange.sol
@@ -3,12 +3,13 @@ pragma solidity ^0.5.0;
 import "openzeppelin-solidity/contracts/math/SafeMath.sol";
 
 contract Exchange {
+    using SafeMath for uint;
 // TODO:
 // [X] Set the fee account
 // [X] Set the fee percent
 // [X] Revert ether if by mistake someone send
-// [] Deposit Ether
-// [] Withdraw Ether
+// [X] Deposit Ether
+// [X] Withdraw Ether
 // [] Deposit Tokens
 // [] Withdraw Tokens
 // [] Check Balances
@@ -21,8 +22,12 @@ contract Exchange {
     address public feeAccount;
     uint256 public feePercent;
 
-    address constant ETHER_ADD = address(0);
+    address constant ETHER_ADDRESS = address(0);
     mapping (address => mapping(address => uint256)) public tokens;
+
+    // Events
+    event Deposit(address token, address user, uint256 amount, uint256 balance);
+    event Withdraw(address token, address user, uint256 amount, uint256 balance);
 
     constructor(address _feeAccount, uint256 _feePercent) public {
         feeAccount = _feeAccount;
@@ -32,5 +37,17 @@ contract Exchange {
     // Fallback: revers if Ether is sent to this smart contract by mistake
     function() external {
         revert();
+    }
+
+    function depositEther() payable public {
+        tokens[ETHER_ADDRESS][msg.sender] = tokens[ETHER_ADDRESS][msg.sender].add(msg.value);
+        emit Deposit(ETHER_ADDRESS, msg.sender, msg.value, tokens[ETHER_ADDRESS][msg.sender]);
+    }
+
+    function withdarwEther(uint256 _amount) public {
+        require(_amount <= tokens[ETHER_ADDRESS][msg.sender]);
+        tokens[ETHER_ADDRESS][msg.sender] = tokens[ETHER_ADDRESS][msg.sender].sub(_amount);
+        msg.sender.transfer(_amount);
+        emit Withdraw(ETHER_ADDRESS, msg.sender, _amount, tokens[ETHER_ADDRESS][msg.sender]);
     }
 }
--- test/Exchange.test.js
+++ test/Exchange.test.js
@@ -1,5 +1,5 @@
 const Exchange = artifacts.require('Exchange');
-import {EVM_REVERT, tokens} from './helpers';
+import {EVM_REVERT, tokens, ether, ETHER_ADDRESS} from './helpers';
 const Token = artifacts.require('Token');
 require('chai').use(require('chai-as-promised')).should();
 
@@ -33,5 +33,60 @@ contract('Exchange', ([deployer, feeAccount, user1]) => {
         it('Revert ether if someone send', async () => {
             await exchange.sendTransaction({value: 1, from: user1}).should.be.rejectedWith(EVM_REVERT);
         });
-    })
+    });
+
+    describe('Deposit Ether', () => {
+        let result;
+        let amount;
+
+        beforeEach(async () => {
+            amount = ether(10);
+            result = await exchange.depositEther({from: user1, value: amount});
+        });
+
+        it('tracks the ether balances', async () => {
+            const balances = await exchange.tokens(ETHER_ADDRESS, user1);
+            balances.toString().should.equal(amount.toString());
+        });
+
+        it('emits the deposit ether event', async () => {
+            const log = result.logs[0];
+            log.event.should.equal('Deposit');
+            const event = log.args;
+            event.token.should.equal(ETHER_ADDRESS);
+            event.user.should.equal(user1);
+            event.amount.toString().should.equal(amount.toString());
+            event.balance.toString().should.equal(amount.toString());
+        })
+    });
+
+    describe('Withdraw Tokens', async () => {
+        let amount;
+        let result;
+        beforeEach(async () => {
+            amount = ether(10);
+            await exchange.depositEther({from: user1, value: amount});
+        });
+
+        describe('Withdraw ether funds', () => {
+            beforeEach(async () => {
+                result = await exchange.withdarwEther(amount, {from: user1});
+            });
+
+            it('withdraw ether funds', async () => {
+                const balance = await exchange.tokens(ETHER_ADDRESS, user1);
+                balance.toString().should.equal('0');
+            });
+
+            it('emits the withdraw ether event', async () => {
+                const log = result.logs[0];
+                log.event.should.equal('Withdraw');
+                const event = log.args;
+                event.token.should.equal(ETHER_ADDRESS);
+                event.user.should.equal(user1);
+                event.amount.toString().should.equal(amount.toString());
+                event.balance.toString().should.equal('0');
+            });
+        });
+    });
 });
